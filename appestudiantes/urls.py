44212b87b06bab1396fc93704d05822ec538df07
Jose Tomas Guzman <121651752+Slothomas@users.noreply.github.com>
2024-06-05 18:18:57 -0400

    update urls appestudiantes

--- appestudiantes/urls.py
+++ appestudiantes/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('datosEstadudiante',views.datosEstudiante, name='datosEstudiante'),
+    path('datosEstudiante',views.datosEstudiante, name='datosEstudiante'),
     path('resumeAsignaturas', views.resumeAsignaturas, name ='resumeAsignaturas'),
     path('resumeNotas', views.resumeNotas, name ='resumeNotas'),
     path('proximosFeed', views.proximosFeed, name ='proximosFeed'),  
